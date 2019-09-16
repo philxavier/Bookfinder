66ba6de657354355113ffd68124bdb1fe1f47f0a
Felipe <philipe.s.xavier@gmail.com>
2019-09-16 09:43:54 -0700

    Updates

---
+++ precache-manifest.76c1ccbe36bb617a8f445d16d99b4869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5304e8f116cbc7c8ecdec61903fcc5",
+    "url": "/philxavier/Bookfinder/index.html"
+  },
+  {
+    "revision": "7005e7af579667f89e1b",
+    "url": "/philxavier/Bookfinder/static/css/main.f3a11ab6.chunk.css"
+  },
+  {
+    "revision": "debc50a1db00528259b3",
+    "url": "/philxavier/Bookfinder/static/js/2.d7b78741.chunk.js"
+  },
+  {
+    "revision": "7005e7af579667f89e1b",
+    "url": "/philxavier/Bookfinder/static/js/main.238df23a.chunk.js"
+  },
+  {
+    "revision": "85ecafc3d37d9785d154",
+    "url": "/philxavier/Bookfinder/static/js/runtime~main.6f1077d5.js"
+  }
+]);
